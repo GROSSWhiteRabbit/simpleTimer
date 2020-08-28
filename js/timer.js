52911f986f0b1df2fdb2b657f8ea839a1c66d68c
GrossWhiteRabbit <vowa.kononenko@gmail.com>
2020-08-28 15:58:14 +0300

    28.08.2020 version 1

---
+++ js/timer.js
@@ -0,0 +1,73 @@
+import {getZero} from '../services/services';
+function timer(id, deadline){
+
+
+    function getTimeRemaining(strOrYear, month, days, hours=0, minutes=0, seconds=0){
+
+        const deadline = (typeof(strOrYear) == 'string') ? new Date(strOrYear) : new Date(strOrYear, month, days, hours, minutes, seconds),
+            TimeRemaining = new Date(deadline.getTime() - Date.now()); 
+
+        if (TimeRemaining.getTime() <= 0) {
+            return {
+            t: TimeRemaining.getTime(),
+            days: '00',
+            hours: '00',
+            minutes: '00',
+            seconds: '00',
+            };
+        }
+
+        const daysN = Math.floor(TimeRemaining.getTime()/1000/60/60/24);
+
+        // const t = Date.parse(endtime) - Date.parse(new Date()),
+        // days = Math.floor( (t/(1000*60*60*24)) ),
+        // seconds = Math.floor( (t/1000) % 60 ),
+        // minutes = Math.floor( (t/1000/60) % 60 ),
+        // hours = Math.floor( (t/(1000*60*60) % 24) );
+   
+        return {
+            t: TimeRemaining.getTime(),
+            days:getZero(daysN),
+            hours: getZero(TimeRemaining.getUTCHours()),
+            minutes:  getZero(TimeRemaining.getUTCMinutes()),
+            seconds:  getZero(TimeRemaining.getUTCSeconds()),
+        };
+
+        
+    }
+
+
+
+
+
+
+    function  setClock(id, strOrYear, month, days, hours=0, minutes=0, seconds=0) {
+      const timerId = document.querySelector(id);
+      const daysElem = timerId.querySelector('#days');
+      const hoursElem = timerId.querySelector('#hours');
+      const minutesElem = timerId.querySelector('#minutes');
+      const secondsElem = timerId.querySelector('#seconds');
+      const timer = setInterval(updateClock, 1000);
+      updateClock();
+        function updateClock() {
+
+        const TimeRemainingObj = getTimeRemaining(strOrYear, month, days, hours, minutes, seconds);
+
+
+        daysElem.textContent = TimeRemainingObj.days;
+        hoursElem.textContent = TimeRemainingObj.hours;
+        minutesElem.textContent = TimeRemainingObj.minutes;
+        secondsElem.textContent = TimeRemainingObj.seconds;
+
+        if(TimeRemainingObj.t <= 0){
+        clearInterval(timer);
+        }
+        }
+        
+    }
+    
+  setClock(id, deadline);
+
+}
+
+export default timer;
---
+++ services/services.js
@@ -0,0 +1,28 @@
+const postData = async (url, data) => {
+    const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: data,
+    });
+    return await res.json();
+};
+
+const getResource = async (url) => {
+    const res = await fetch(url);
+    if(!res.ok){
+        throw new Error(`could not feth ${url}, status:${res.status}` );
+    }
+    return await res.json();
+};
+
+function getZero(num){
+    if(num >=0 && num < 10) {
+        return `0${num}`;
+    } else {
+        return `${num}`;
+    }
+}
+
+export {postData, getResource, getZero};
